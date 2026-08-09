8e128f6c00ee8091fabdbc93a938f42811e95e3f
Carlos Daniel Navarro <carlos.navarro.l@uni.pe>
2026-08-09 14:41:36 -0500

    agregando cositas al archivo

---
+++ struct_data.py
@@ -0,0 +1,13 @@
+'''
+Mostrar ejemplos de creación de todas las estructuras soportadas por defecto en python.
+Utiliza operaciones de insercion, borrado, actualizacion y ordenación.
+'''
+
+# List
+my_list = [1,2,3,4,5]
+my_list.append(8)
+my_list.remove(1)
+my_list[0] = 10
+my_list.sort()
+
+print("List:", my_list)
